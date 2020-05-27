9042e4212371a912e0e76d1ff0cb07d620b0925e
Novindra Prasetio <41236930+novindrap07@users.noreply.github.com>
2020-05-27 16:11:58 +0700

    Add files via upload

---
+++ datapipeline_masterAgent.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# Setup
+set -x
+set -e
+PROJECT_ID="urbanindo-data-platform"
+
+# Load Listing Secondary Data
+rm -rf masterAgent.tsv*
+aws s3 cp s3://data.99iddev.net/rumah123-daily-sync/master_agent/masterAgent.schema.json masterAgent.schema.json
+aws s3 cp $1 masterAgent.tsv
+
+# Data Cleaning
+sed -i 's/\\n//g' masterAgent.tsv
+
+# Compress with GZip
+gzip masterAgent.tsv
+
+# Load to BigQuery
+bq load --project_id=$PROJECT_ID \
+    --replace --schema="masterAgent.schema.json" \
+    --source_format=CSV --field_delimiter="\t" \
+    --max_bad_records=10000 production.properties \
+    masterAgent.tsv.gz
