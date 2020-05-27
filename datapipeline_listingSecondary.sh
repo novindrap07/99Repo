ce3a119b67b30748ce081243fe2b6e967c50029d
Novindra Prasetio <41236930+novindrap07@users.noreply.github.com>
2020-05-27 13:54:42 +0700

    Add files via upload

---
+++ datapipeline_listingSecondary.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# Setup
+set -x
+set -e
+PROJECT_ID="urbanindo-data-platform"
+
+# Load Listing Secondary Data
+rm -rf secondary_listing.tsv*
+aws s3 cp s3://data.99iddev.net/rumah123-daily-sync/listing-secondary/listingSecondary.schema.json listingSecondary.schema.json
+aws s3 cp $1 secondary_listing.tsv
+
+# Data Cleaning
+sed -i 's/\\n//g' secondary_listing.tsv
+
+# Compress with GZip
+gzip secondary_listing.tsv
+
+# Load to BigQuery
+bq load --project_id=$PROJECT_ID \
+    --replace --schema="listingSecondary.schema.json" \
+    --source_format=CSV --field_delimiter="\t" \
+    --max_bad_records=10000 production.properties \
+    secondary_listing.tsv.gz
