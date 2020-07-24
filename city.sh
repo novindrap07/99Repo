883944f5865c574c258d3afd0c8bfa3324ad59eb
Novindra Prasetio <41236930+novindrap07@users.noreply.github.com>
2020-07-24 13:08:50 +0700

    Script of Load to BQ

---
+++ city.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+set -ex
+
+PROJECT_ID="urbanindo-data-platform"
+DATASET="r123"
+
+sudo yum -y install jq
+
+function load_to_bq() {
+    TABLE=$1
+
+    rm -rf ${TABLE}.tsv*
+    bq show --format=prettyjson ${PROJECT_ID}:${DATASET}.${TABLE} | jq '.schema.fields' > ${TABLE}.schema.json
+    aws s3 cp $2 ${TABLE}.tsv
+    sed -i 's/\\n//g' ${TABLE}.tsv
+    gzip ${TABLE}.tsv
+    bq load --project_id=$PROJECT_ID \
+        --replace --schema="${TABLE}.schema.json" \
+        --source_format=CSV --field_delimiter="\t" \
+        ${DATASET}.${TABLE} \
+        ${TABLE}.tsv.gz
+}
+
+# Load Primary Listing
+load_to_bq City $1
